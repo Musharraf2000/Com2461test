7332a0aee04dac9c8b216555eb3975f98838f5ab
Musharraf2000 <musharraf.azhar19@gmail.com>
2024-08-21 11:35:50 +0530

    third commit

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,25 @@
+// main.js
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
+
+
+
+
+
+
+
+
 console.log("Hello");
 
 let inputfield1 = document.getElementById("hoursinput");
